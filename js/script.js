7e081bd6f501f801c226b60028a732a014649263
Aleksey Grechihin <9108921170@dz-led.ru>
2020-09-04 21:24:47 +0300

    22

---
+++ js/script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+class ToDo {
+  constructor(form, input, todoList, todoCompleted) {
+    this.form = document.querySelector(form);
+    this.input = document.querySelector(input);
+    this.todoList = document.querySelector(todoList);
+    this.todoCompleted = document.querySelector(todoCompleted);
+
+    this.todoData = new Map(JSON.parse(localStorage.getItem("toDoList")));
+  }
+
+  addToStorage() {
+    localStorage.setItem("toDoList", JSON.stringify([...this.todoData]));
+  }
+
+  render() {
+    this.todoList.textContent = "";
+    this.todoCompleted.textContent = "";
+    this.todoData.forEach(this.createItem, this);
+    this.addToStorage();
+  }
+
+  createItem(todo) {
+    const li = document.createElement("li");
+    li.classList.add("todo-item");
+    li.key = todo.key;
+    li.insertAdjacentHTML(
+      "beforeend",
+      `
+        <span class="text-todo">${todo.value}</span>
+            <div class="todo-buttons">
+                <button class="todo-remove"></button>
+                <button class="todo-complete"></button>
+            </div>
+    `
+    );
+    if (todo.completed) {
+      this.todoCompleted.append(li);
+    } else {
+      this.todoList.append(li);
+    }
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    if (this.input.value.trim()) {
+      const newTodo = {
+        value: this.input.value,
+        completed: false,
+        key: this.generateKey(),
+      };
+      this.todoData.set(newTodo.key, newTodo);
+      this.render();
+    }
+  }
+
+  generateKey() {
+    return (
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15)
+    );
+  }
+
+  deleteItem() {}
+
+  completedItem() {
+    this.todoData.forEach(function (item) {
+      if (item.key === li.key) {
+        console.log(1);
+      }
+    });
+  }
+
+  hendler() {
+    let todoContainer = document.querySelector(".todo-container");
+    todoContainer.addEventListener("click", (event) => {
+      const target = event.target;
+
+      if (target.closest(".todo-complete")) {
+        this.completedItem();
+      } else if (target.closest(".todo-remove")) {
+        this.deleteItem();
+      }
+      this.render();
+    });
+  }
+
+  init() {
+    this.form.addEventListener("submit", this.addTodo.bind(this));
+    this.render();
+  }
+}
+
+const todo = new ToDo(
+  ".todo-control",
+  ".header-input",
+  ".todo-list",
+  ".todo-completed"
+);
+todo.hendler();
+todo.init();
